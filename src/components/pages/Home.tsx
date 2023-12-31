31020b80388387785ba06b87d1de2d6fe9dbf45c
Rui Soares <rui.rprs3@gmail.com>
2023-12-31 18:48:00 +0000

    update versão estavel

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Links from '../components/Links';
 
 interface Props {
@@ -7,6 +8,16 @@ interface Props {
 }
 
 const Home = ({ items, icons, links }: Props) => {
+    React.useEffect(() => {
+        // Disable scroll when component mounts
+        document.body.style.overflow = 'hidden';
+
+        // Re-enable scroll when component unmounts
+        return () => {
+            document.body.style.overflow = 'visible';
+        };
+    }, []);
+
     return (
         <>
             <Links items={items} icons={icons} links={links} />
